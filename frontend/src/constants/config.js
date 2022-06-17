c54532189f8c3a18d9f6b84adfb0ec21085944bb
webdesigntuts <hostelidis99@gmail.com>
2022-06-17 19:12:12 +0300

    URL

--- frontend/src/constants/config.js
+++ frontend/src/constants/config.js
@@ -4,6 +4,6 @@ const queryClient = new QueryClient();
 //CHAGE THIS TO YOUR OWN API
 const AXIOS_URL =
   process.env.NODE_ENV === "production"
-    ? "http://expensetracker20/api/"
+    ? "https://expensetracker20/api/"
     : "http://localhost:5000/api/";
 export { AXIOS_URL, queryClient };
